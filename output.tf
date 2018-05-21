95d88ee47134aa8e3b538f8d148b98fec3d9cc17
Dmitry Tsoy <hd.deman@gmail.com>
2018-05-21 15:38:04 +0300

    fix outputs

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output minions {
   value = ["${aws_instance.minion.*.id}"]
 }
 
+output master_private_ip {
+  value = "${element(aws_instance.master.*.private_ip, 0)}"
+}
+
 output minion_security_group {
   value = "${aws_security_group.minions.id}"
 }
@@ -14,10 +18,18 @@ output minion_elb_zone_id {
   value = "${aws_elb.minion.zone_id}"
 }
 
+output minion_elb_id {
+  value = "${aws_elb.minion.id}"
+}
+
+output minion_aws_iam_instance_profile_name {
+  value = "${aws_iam_instance_profile.minions.name}"
+}
+
 output minion_elb_tcp_dns_name {
-  value = "${element(aws_elb.minion-tcp.*.dns_name, 0)}"
+  value = "${join(" ", aws_elb.minion-tcp.*.dns_name)}"
 }
 
 output minion_elb_tcp_zone_id {
-  value = "${element(aws_elb.minion-tcp.*.zone_id, 0)}"
+  value = "${join(" ", aws_elb.minion-tcp.*.zone_id)}"
 }
